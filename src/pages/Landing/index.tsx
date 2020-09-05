e4726c10169ca29db9e819640613aff3844e9e33
Rishi Kothari <itsrishikothari@gmail.com>
2020-09-05 15:28:48 -0400

    Test page

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text } from '@geist-ui/react'
+import { AppLayout } from '../../layouts'
+
+const LandingPage: React.FC = () => (
+	<AppLayout>
+		<Text>Hello there!</Text>
+	</AppLayout>
+)
+
+export default LandingPage
